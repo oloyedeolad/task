6c89175176e4c2dc551b05bc8d5b8567099179da
ola <oloyedeoladimeji@hotmail.com>
2022-08-12 09:40:07 +0100

    configure

---
+++ shared/src/test/java/com/oi/shared/SharedApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.oi.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
